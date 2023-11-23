1988826025596b1070687ccadc3feb1326b4e8db
Felix <spasst.spiel@gmail.com>
2023-11-23 01:19:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let footerElements = document.getElementsByClassName("footer");
 console.log(footerElements);
 
 
-let version = "1.4";
-let build = "1E1";
+let version = "1.4 Beta 1";
+let build = "1E1a";
 
 var returnString = `Version ${version} (Build ${build}) \n ©2023 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
