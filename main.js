318d2aa6524c5479c850cc7abf474d9ea1afca45
nocontent06 <spasst.spiel@gmail.com>
2024-06-02 19:26:50 +0200

    Added more iPads

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ console.log(footerElements);
 
 let version = "1.4.2";
 let build = "1E3";
-let commit = "5"
+let commit = "6"
 
 var returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
