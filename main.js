a3811847068402330e0870937e2fc9d1ab71d724
nocontent06 <spasst.spiel@gmail.com>
2024-10-28 00:47:18 +0100

    Updated iMacs, iOS, iPads (added iPad Mini 7) iPhone OS

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ console.log(footerElements);
 
 let version = "1.4.2";
 let build = "1E3";
-let commit = "9"
+let commit = "10 (2024-10-27)"
 
 var returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
