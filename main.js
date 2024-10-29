d0a224bcb4ea6bb7d8edcc29d45d3330367a4ee3
Felix <spasst.spiel@gmail.com>
2024-10-29 01:34:33 +0100

    Updated some iPhones up to iPhone 5 China, started iPhone 5c GSM & CDMA

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ console.log(footerElements);
 
 let version = "1.4.2";
 let build = "1E3";
-let commit = "10 (2024-10-27)"
+let commit = "11 (2024-10-29)"
 
 var returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
