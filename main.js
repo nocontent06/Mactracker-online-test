4c4ba9fd0d4883fd039fdf7c6968fc2cf8c5adb0
Felix <spasst.spiel@gmail.com>
2024-11-09 00:01:45 +0100

    Updated iPhones up to XR (continue with 11)

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log(footerElements);
 
 
 let version = "1.4.2";
-let build = "1E3n";
-let commit = "14 (2024-11-08)"
+let build = "1E3o";
+let commit = "15 (2024-11-08)"
 
 var returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
