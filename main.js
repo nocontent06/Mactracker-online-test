259caf95c772947b482e277ac6107ad278795f05
Felix <spasst.spiel@gmail.com>
2024-11-08 22:49:44 +0100

    Updated Version for the iPhone 7 Plus commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log(footerElements);
 
 
 let version = "1.4.2";
-let build = "1E3m";
-let commit = "13 (2024-11-07)"
+let build = "1E3n";
+let commit = "14 (2024-11-08)"
 
 var returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
