b85accbd54104a7ad801cf500489bcf9e2fb2b5a
Felix <spasst.spiel@gmail.com>
2024-11-12 11:08:03 +0100

    Updated up to iPhone SE 3. Continue with iPhone 14

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
 let build = "1E3r";
-let commit = "18 (2024-11-11)"
+let commit = "19 (2024-11-12)"
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2024 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
