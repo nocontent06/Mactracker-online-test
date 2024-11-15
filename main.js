22f0e0873b12a955e8c6825696a1b780b76e4cc5
Felix <spasst.spiel@gmail.com>
2024-11-15 21:24:02 +0100

    Updated iPods up to 5th Gen L2006 (continue with 5th Gen L2006 U2)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3u";
-let commit = "21 (2024-11-15)"
+let build = "1E3v";
+let commit = "22 (2024-11-15)"
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2024 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
