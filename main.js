02d5ce1d0a7115ef2456e9997a1a7e4d1e2c47df
Felix <spasst.spiel@gmail.com>
2024-11-15 01:28:06 +0100

    Finished iPhones

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3s";
-let commit = "19 (2024-11-15)"
+let build = "1E3t";
+let commit = "20 (2024-11-15)"
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2024 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
