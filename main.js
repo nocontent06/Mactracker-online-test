a03147ed2c8d841170ffcf9651dba1e3dcf9ffd4
Felix <spasst.spiel@gmail.com>
2024-11-18 00:08:48 +0100

    Updated Mac Minis after Merge

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3w";
-let commit = "23 (2024-11-17)"
+let build = "1E3x";
+let commit = "24 (2024-11-17)"
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2024 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
