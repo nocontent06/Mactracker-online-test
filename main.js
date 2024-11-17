cc9341576238a4c56b28c05d1dbbfca61dd28b0b
Felix <spasst.spiel@gmail.com>
2024-11-17 19:14:24 +0100

    Updated all iPods

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3v";
-let commit = "22 (2024-11-15)"
+let build = "1E3w";
+let commit = "23 (2024-11-17)"
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2024 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
