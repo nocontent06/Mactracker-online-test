ca135f02d2c7381f7ab5201cbd140eb5358b28c3
nocontent06 <spasst.spiel@gmail.com>
2025-01-01 12:25:37 +0100

    Updated MacBook Pros up to 13-inch Mid 2009

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3ad";
-let commit = "30 (2024-12-31)";
+let build = "1E3ae";
+let commit = "31 (2025-01-01)";
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2025 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
