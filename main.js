001bec76cf0a9fe59e24e2dd4509f01c22e591ae
nocontent06 <spasst.spiel@gmail.com>
2025-01-09 13:38:08 +0100

    Fixed some issues/types

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3ag";
-let commit = "33 (2025-01-09)";
+let build = "1E3ah";
+let commit = "34 (2025-01-09)";
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2025 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
