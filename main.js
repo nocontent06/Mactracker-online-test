82e77da7cf37eadcce5fa7589d31bb5929b7b5e9
NoContent_06 <spasst.spiel@gmail.com>
2025-03-07 15:56:27 +0100

    Updated up to MBP 13 inch 2020 2TB3

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3ag";
-let commit = "35 (2025-02-09)";
+let build = "1E3ah";
+let commit = "36 (2025-03-07)";
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2025 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
