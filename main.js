26be7252454c4721d1e066a4c632dd10e53a299e
nocontent06 <spasst.spiel@gmail.com>
2025-03-09 22:56:27 +0100

    Updated up to MBP 14 inch M1 Pro/Max

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3ah";
-let commit = "36 (2025-03-07)";
+let build = "1E3ai";
+let commit = "37 (2025-03-09)";
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2025 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
