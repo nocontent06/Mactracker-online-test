f0389c28b9637a03cb42e1f6f9681f56cfa867f3
nocontent06 <spasst.spiel@gmail.com>
2025-03-12 22:05:06 +0100

    Updated Apple TVs from some Typos as well as iMacs and combined 2- and 4-port iMacs with M-SoC

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3ak";
-let commit = "38 (2025-03-10)";
+let build = "1E3al";
+let commit = "39 (2025-03-12)";
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2025 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
