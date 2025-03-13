2aa91f9cfb54dbc90e501424e1faf4e7bf1983bc
nocontent06 <spasst.spiel@gmail.com>
2025-03-13 23:45:15 +0100

    Added the new Mac Studio

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let version = "1.4.2";
-let build = "1E3am";
-let commit = "40 (2025-03-13)";
+let build = "1E3an";
+let commit = "41 (2025-03-14)";
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2025 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
